d86824cc40f2bc260c8e8829d1948c5159b913f3
igorsgallo <56000788+igorsgallo@users.noreply.github.com>
2019-09-30 17:08:09 -0300

    Add files via upload

---
+++ Capstone_project.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ### Capstone project for Coursera
+# 
+# This notebook is part of the "Applied Data Science Capstone" final project
+
+# In[5]:
+
+
+import pandas as pd
+import numpy as np
+
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
